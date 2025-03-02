dac18cd8b26f407623d32ae1246d598a77840f19
AKHILESH YADAV <122615318+akhilesh3841@users.noreply.github.com>
2025-03-02 20:01:49 +0530

    Update Request.jsx

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -26,7 +26,7 @@ const Request = () => {
 
   const handlerequest = async () => {
     try {
-      const response = await axios.get("/api/userdata/pending/recived", {
+      const response = await axios.get("http://localhost:3000/userdata/pending/recived", {
         withCredentials: true,
       });
 
